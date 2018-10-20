04daab616701bca18953bdda755b9578eb5419e6
Michael Franke <the.secret.goldfish@me.com>
2018-10-20 07:17:14 +0200

    towards appr. optimal trial selection in part II of stanford pilot

--- helper.js
+++ helper.js
@@ -78,3 +78,91 @@ var drawOnCanvas = function(canvasElem, trialInfo, displayType) {
         }
     }
 };
+
+
+// returns an approximately optimal stimulus based on a production trial
+var sampleFocalNumber = function(productionTrial) {
+	// dev file for opt exp design 4 Stanford pilot
+
+	// total set size
+	var TSS = 7;
+
+	// global error probability
+	var eps = 0.01
+
+	// all hypotheses (= all pairs (u,l) of lower+upper bound with 0 <= l <= u <= TSS )
+	var hypotheses = _.flatten(_.map(_.range(TSS+1), 
+									 function(lower) {return(_.map(_.range(TSS+1-lower), 
+																   function(upper) {return([lower,lower+upper])}))} ));
+
+	// dummy example matrix of observations (rows = true/false judgements, cols = state (0,1,...,TSS), entry: number of observed judgements)
+	var observations = [[0,0,1,0,0,1,0,0],
+						[1,0,0,0,0,0,1,1]];
+
+	var vector_sum = function(vector) {
+		var sum = 0
+		_.map(vector, function(i) {sum = sum+i})
+		return(sum)
+	}
+
+	var vector_prod = function(vector) {
+		var prod = 1
+		_.map(vector, function(i) {prod = prod*i})
+		return(prod)
+	}
+
+	var vector_normalize = function(vector) {
+		var v_sum = vector_sum(vector)
+		return(_.map(vector, function(i) {return(i/v_sum)}))
+	}
+
+	var get_likelihoods = function(observations) {
+		var llh = _.map(hypotheses, function(h) {
+							return(vector_prod(_.map(_.range(TSS+1), function(s) {
+										var true_description = h[0] <= s && s <= h[1]
+										var true_factor  =  observations[0][s] == 0 ? 1 : true_description ? observations[0][s] * (1-eps) : observations[0][s] * eps
+										var false_factor = observations[1][s] == 0 ? 1 : !true_description ? observations[1][s] * (1-eps) : observations[1][s] * eps
+										return(true_factor * false_factor)
+									})))
+					})
+		return(llh)
+
+	}
+
+	var posterior = vector_normalize(get_likelihoods(observations))
+
+	var get_state_predictions = function(posterior) {
+		return(_.map(_.range(TSS+1), function(s) {
+			// weighted sum over all hypotheses 
+			return(vector_sum(_.map(_.range(hypotheses.length), function(hindex) {
+				var h = hypotheses[hindex];
+				var hPost = posterior[hindex];	
+				var prob_true = h[0] <= s && s <= h[1] ? 1-eps : eps
+				return(hPost * prob_true)
+			})))}))
+	}
+
+	var state_predictions = get_state_predictions(posterior)
+
+	var discrete_sample = function(prob_vector) {
+		prob_vector = vector_normalize(prob_vector)
+		var cumulative_vector = _.map(_.range(prob_vector.length), function(i) {
+			return(vector_sum(_.slice(prob_vector, 0, i+1)))
+		})
+		var my_sample = _.random(0,1.0, true)
+		var boolean_vector = _.map(cumulative_vector, function(i) {
+			return(i < my_sample)
+		})
+		return(vector_sum(boolean_vector))
+	}
+
+	var softmax_sample = function(state_predictions, alpha) {
+		var softmax_probs = vector_normalize(_.map(state_predictions, function(s) {
+			return(Math.exp(alpha * (0.5 - Math.abs(s-0.5))))
+		}))
+		return(discrete_sample(softmax_probs))
+	//	return(discrete_sample(_.range(TSS+1), softmax_probs))
+	}
+
+	softmax_sample(state_predictions, 10)
+}
